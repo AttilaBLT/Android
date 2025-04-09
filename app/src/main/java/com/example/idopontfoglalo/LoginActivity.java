ed69f53161bcb2d0802a083feb02a4c5e7beff35
Jánovszki Attila <h263503@stud.u-szeged.hu>
2025-04-10 00:49:07 +0200

    Cleaning

--- app/src/main/java/com/example/idopontfoglalo/LoginActivity.java
+++ app/src/main/java/com/example/idopontfoglalo/LoginActivity.java
@@ -73,24 +73,6 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG, "onStart");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(TAG, "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(TAG, "onDestroy");
-    }
-
-    @Override
     protected void onPause() {
         super.onPause();
 
@@ -99,13 +81,6 @@ public class LoginActivity extends AppCompatActivity {
         editor.putString("password", passwordET.getText().toString());
         editor.apply();
 
-
         Log.i(TAG, "onPause");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG, "onResume");
     }
 }
--- app/src/main/java/com/example/idopontfoglalo/RegisterActivity.java
+++ app/src/main/java/com/example/idopontfoglalo/RegisterActivity.java
@@ -92,34 +92,4 @@ public class RegisterActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainPageActivity.class);
         startActivity(intent);
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG, "onStart");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(TAG, "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(TAG, "onDestroy");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i(TAG, "onPause");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG, "onResume");
-    }
 }
